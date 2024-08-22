a0b08c277fea4a2c23cd96ed8714ba85c81e49bf
Rahman <rahmanaltaj1@gmail.com>
2024-08-22 15:19:49 +0300

    add react FrameWork2

---
+++ resources/js/Pages/Speakers.jsx
@@ -0,0 +1,13 @@
+
+import React    from "react";
+import NavBarPages from "../components/NavBarPages.jsx";
+
+export default function Speakers() {
+
+    return (
+        <div>
+            <NavBarPages/>
+            <h1>Speakers</h1>
+        </div>
+    );
+}
---
+++ resources/js/components/HandelPages.jsx
@@ -0,0 +1,17 @@
+
+import React     from "react";
+import NavBarPages from "./NavBarPages.jsx";
+
+import HomePage from "../Pages/HomePage.jsx";
+
+
+export default function HandelPages() {
+    return (
+
+        <div>
+            <NavBarPages/>
+            <HomePage/>
+
+        </div>
+    );
+}
